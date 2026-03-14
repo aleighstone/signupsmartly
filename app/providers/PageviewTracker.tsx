b7f28082750a8a996fa19d675c75181a6c44a812
Allison Stone <allisonleighstone@gmail.com>
2026-03-14 14:48:23 -0700

    Fix useSearchParams prerender: use window.location in PageviewTracker

--- app/providers/PageviewTracker.tsx
+++ app/providers/PageviewTracker.tsx
@@ -1,25 +1,19 @@
 'use client';
 
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 import { usePostHog } from '@posthog/react';
 
 export function PageviewTracker() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
   const posthog = usePostHog();
 
   useEffect(() => {
     if (pathname && posthog) {
-      const url =
-        typeof window !== 'undefined'
-          ? window.location.href
-          : searchParams?.toString()
-            ? `${pathname}?${searchParams.toString()}`
-            : pathname;
+      const url = typeof window !== 'undefined' ? window.location.href : pathname;
       posthog.capture('$pageview', { $current_url: url });
     }
-  }, [pathname, searchParams, posthog]);
+  }, [pathname, posthog]);
 
   return null;
 }
